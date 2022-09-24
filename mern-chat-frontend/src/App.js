4ff07732a5e1163fc191336c2c24815e259a2e33
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-09-24 16:14:41 +0200

    mise en place de l'interface d'accueil

--- mern-chat-frontend/src/App.js
+++ mern-chat-frontend/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
 
 function App() {
-    return <div>
-        <Navigation/>
-    </div>
+    return (
+        <BrowserRouter>
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/chat" element={<Chat />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
